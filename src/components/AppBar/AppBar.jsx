00133072171c89bca114b1334c9f02f58130b32c
tapsikk <tapsgame@gmail.com>
2024-05-10 00:49:11 +0300

    ok

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -2,7 +2,7 @@
 import { Navigation } from '../Navigation/Navigation';
 import { UserMenu } from '../UserMenu/UserMenu';
 import { useSelector } from 'react-redux';
-import AuthNav  from '../authNav/AuthNav';
+import AuthNav  from '../AuthNav/AuthNav';
 import { selectIsLoggedIn } from '../../redux/auth/selectors';
 import css from './AppBar.module.css';
 
