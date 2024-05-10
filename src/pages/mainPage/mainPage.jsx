120874fa2b4203e913a8d08e2057977a604dde33
tapsikk <tapsgame@gmail.com>
2024-05-10 17:43:58 +0300

    123

--- src/pages/mainPage/mainPage.jsx
+++ src/pages/mainPage/mainPage.jsx
@@ -1,24 +1,23 @@
-// import DocumentTitle from '../../DocumentTitle';
-import style from './mainPage'
+
 const styles = {
   container: {
     minHeight: 'calc(100vh - 50px)',
     display: 'flex',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#242424', 
   },
   title: {
     fontWeight: 500,
     fontSize: 48,
     textAlign: 'center',
+    color: 'white', 
   },
 };
 
 export default function HomePage() {
   return (
     <>
-      {/* <DocumentTitle>Home</DocumentTitle> */}
-
       <div style={styles.container}>
         <h1 style={styles.title}>
           Task manager welcome page{' '}
