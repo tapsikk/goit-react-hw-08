90930c1c3e893ca94e9e7733a9f1d65ecaadbfc2
tapsikk <tapsgame@gmail.com>
2024-05-11 23:28:19 +0300

    123

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
-import { Suspense, lazy, useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { Navigate, Route, Routes } from 'react-router-dom';
-import { RestrictedRoute } from './RestrictedRoute';
-import { refreshUser } from './redux/auth/operations';
-import { Layout } from './components/Layout';
-import { selectIsLoggedIn, selectIsRefreshing } from './redux/auth/selectors';
+import { Suspense, lazy, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { RestrictedRoute } from "./RestrictedRoute";
+import { refreshUser } from "./redux/auth/operations";
+import { Layout } from "./components/Layout";
+import { selectIsLoggedIn, selectIsRefreshing } from "./redux/auth/selectors";
 
-const ContactsPage = lazy(() => import('./pages/ContactsPage/ContactsPage'));
-const MainPage = lazy(() => import('./pages/mainPage/mainPage'));
-const RegisterPage = lazy(() => import('./pages/RegistrationPage/RegistrationForm'));
-const LoginPage = lazy(() => import('./pages/loginPage/LoginForm'));
+const ContactsPage = lazy(() => import("./pages/ContactsPage/ContactsPage"));
+const MainPage = lazy(() => import("./pages/mainPage/mainPage"));
+const RegisterPage = lazy(() =>
+  import("./pages/RegistrationPage/RegistrationForm")
+);
+const LoginPage = lazy(() => import("./pages/loginPage/LoginForm"));
 
 export const App = () => {
   const dispatch = useDispatch();
@@ -17,7 +19,7 @@ export const App = () => {
   const isRefreshing = useSelector(selectIsRefreshing);
 
   useEffect(() => {
-    const persistedToken = localStorage.getItem('token');
+    const persistedToken = localStorage.getItem("token");
     if (persistedToken) {
       dispatch(refreshUser());
     }
@@ -32,10 +34,38 @@ export const App = () => {
           <Layout isLoggedIn={isLoggedIn}>
             <Routes>
               <Route path="/" element={<MainPage />} />
-              <Route path="/home" element={isLoggedIn ? <ContactsPage /> : <Navigate to="/" />} />
-              <Route path="/register" element={<RestrictedRoute redirectTo="/home" component={<RegisterPage />} />} />
-              <Route path="/login" element={<RestrictedRoute redirectTo="/home" component={<LoginPage />} />} />
-              <Route path="*" element={isLoggedIn ? <Navigate to="/home" /> : <Navigate to="/login" />} />
+              <Route
+                path="/home"
+                element={isLoggedIn ? <ContactsPage /> : <Navigate to="/" />}
+              />
+              <Route
+                path="/register"
+                element={
+                  <RestrictedRoute
+                    redirectTo="/home"
+                    component={<RegisterPage />}
+                  />
+                }
+              />
+              <Route
+                path="/login"
+                element={
+                  <RestrictedRoute
+                    redirectTo="/home"
+                    component={<LoginPage />}
+                  />
+                }
+              />
+              <Route
+                path="*"
+                element={
+                  isLoggedIn ? (
+                    <Navigate to="/home" />
+                  ) : (
+                    <Navigate to="/login" />
+                  )
+                }
+              />
             </Routes>
           </Layout>
         </Suspense>
