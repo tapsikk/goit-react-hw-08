e7b999fb6baf630137ffa6410c463e74d88cda71
tapsikk <tapsgame@gmail.com>
2024-05-11 16:59:24 +0300

    123

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,13 +1,10 @@
 import { Suspense } from 'react';
 import { AppBar } from './AppBar/AppBar';
-import Navigation from './Navigation/Navigation';
 
-export const Layout = ({ children, isLoggedIn, onLogout }) => {
+export const Layout = ({ children }) => {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
-      <AppBar>
-        <Navigation isLoggedIn={isLoggedIn} onLogout={onLogout} />
-      </AppBar>
+      <AppBar/>
       <Suspense fallback={null}>{children}</Suspense>
     </div>
   );
